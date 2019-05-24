72dcaa9cad3dc67a1734ea54bc50390df4338801
Vincent Honca <vincenth1985@gmail.com>
2019-05-24 11:08:39 +0200

    making Shopping Cart class

---
+++ src/com/journaldev/design/strategy/ShoppingCart.java
@@ -0,0 +1,27 @@
+package com.journaldev.design.strategy;
+
+public class ShoppingCart {
+
+
+    public ShoppingCart() {
+    }
+
+    public void addItem(Item item){
+
+    }
+
+    public void removeItem(Item item){
+
+    }
+
+    public int calculateTotal(){
+        return 0;
+    }
+
+    public void pay(PaymentStrategy){
+
+    }
+
+
+
+}
